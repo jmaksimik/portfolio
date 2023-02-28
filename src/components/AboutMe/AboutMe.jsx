b323c11910b5e55d12e8a09c1257c44417e8d9d0
jmaksimik <jmaks@me.com>
2023-02-27 21:55:36 -0500

    Refactor language image array and remove databases from skills

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -12,7 +12,6 @@ export default function AboutMe() {
                 <PortfolioPic />
             </div>
             <LanguageImages />
-            <a target="_blank" href="https://icons8.com/icon/3tC9EQumUAuq/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
         </div>
     )
 }
--- src/components/LanguageImages/LanguageImages.jsx
+++ src/components/LanguageImages/LanguageImages.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
+const languageList = [
+    {source: 'src/assets/images/CSS.png', description: 'CSS'},
+    {source: 'src/assets/images/JS.png', description: 'JS'},
+    {source: 'src/assets/images/Python.png', description: 'Python' },
+    {source: 'src/assets/images/Node.png', description: 'Node.js'},
+    {source: 'src/assets/images/React.png', description: 'React' }
+
+]
+
 export default function LanguageImages(){
     return (
-        <div className='flex-row columns-7'>
-            <img src='src/assets/images/CSS.png' alt='CSS' />
-            <img src='src/assets/images/JS.png' alt='JS'/>
-            <img src='src/assets/images/MongoDB.jpg' alt='MongoDB' />
-            <img src='src/assets/images/Python.png' alt='Python' />
-            <img src='src/assets/images/Node.png' alt='Node.js' />
-            <img src ='src/assets/images/React.png' alt='React' />
-            <img src='src/assets/images/Django.png' alt='Django' />
+        <div className='flex-row columns-5 mt-10'>
+            {languageList.map((language, index) => {
+                return (
+                    <img src={language.source} alt={language.description} className='language-icons' key={index} />
+                )
+            }
+        )}
         </div>
+
+        // <div className='flex-row columns-6 mt-10'>
+        //     <img src='src/assets/images/CSS.png' alt='CSS' />
+        //     <img src='src/assets/images/JS.png' alt='JS'/>
+        //     <img src='src/assets/images/MongoDB.jpg' alt='MongoDB' />
+        //     <img src='src/assets/images/Python.png' alt='Python' />
+        //     <img src='src/assets/images/Node.png' alt='Node.js' />
+        //     <img src ='src/assets/images/React.png' alt='React' />
+        // </div>
     )
 }
