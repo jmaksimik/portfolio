4109f3cfd2faa6e0cf71d9895a3f42db3e212bc8
jmaksimik <jmaks@me.com>
2023-03-03 21:03:35 -0500

    Add custom render of PDF to portfolio/resume page

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,23 +1,34 @@
-import React, {useState} from 'react';
-import {Document, Page} from 'react-pdf/dist/esm/entry.vite';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
+const options = {
+    cMapUrl: 'cmaps/',
+    cMapPacked: true,
+    standardFontDataUrl: 'standard_fonts/',
+};
+
+
 export default function Resume(props) {
+    const [file, setFile] = useState('../../src/assets/jm-resume.pdf')
     const [numPages, setNumPages] = useState(null);
 
-    function onDocumentLoadSuccess({numPages}) {
-        setNumPages(numPages);
+    function onDocumentLoadSuccess({numPages: nextNumPages}){
+        setNumPages(nextNumPages);
     }
 
-    const { pdf } = props;
 
     return (
-            <Document
-                file={pdf}
-                onLoadSuccess = {onDocumentLoadSuccess}
-            >
-             </Document>
+        <div className='container mx-w-full mt-32 flex flex-col content-center'>
+            <p className='text-3xl'>Resume</p>
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+                {Array.from(new Array(numPages), (el, index) => (
+                    <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                ))}
+            </Document>
+        </div>
+
 
     )
 }
