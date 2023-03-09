ae39e2f02e1053f39b735f285046bfd41fb54726
jmaksimik <jmaks@me.com>
2023-03-08 21:13:00 -0500

    Fix path for resume

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -3,13 +3,11 @@ import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import Resume from "../../components/Resume/Resume";
 
-import jmResume from '/jm-resume.pdf';
-
 export default function ResumePage() {
     return (
         <div>
             <NavBar />
-            <Resume pdf={jmResume} />
+            <Resume />
         </div>
     )
 }
